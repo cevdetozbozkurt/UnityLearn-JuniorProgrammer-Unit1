87b9af6b55f20a24369f0e6cf6607dc1f8bacc2f
Cevdet Eren <82716289+cevdetozbozkurt@users.noreply.github.com>
2022-12-19 10:18:06 +0300

    Fixed bugs in challenge

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
+    public GameObject propeller; 
+    private float speed = 0;
+    private float rotationSpeed = 100;
+    private float verticalInput;
+    
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +24,12 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        
+        //Plane's propeller spin
+        propeller.transform.Rotate(new Vector3(0, 0, 30));
     }
 }
