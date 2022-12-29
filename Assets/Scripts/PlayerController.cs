09738f1124feb51941cafa490af09043565b1e04
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-12-29 11:08:58 +0300

    Added new project folders, playerController script and gameobjects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,14 +13,19 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    public string inputID;
 
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
 
     // Update is called once per frame
     void Update()
     {
         //get input from user
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
         //move forward 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput );
         //turn func
